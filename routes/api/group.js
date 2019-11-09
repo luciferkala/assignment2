8a1a85107ff178ab2b0b46ea4b0a72e45848486f
양정훈 <luciferkala@gmail.com>
2019-11-09 14:59:16 +0900

    change code with json

--- routes/api/group.js
+++ routes/api/group.js
@@ -4,39 +4,29 @@ const csvManager = require("../../modules/csvManager");
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  let sGroup = [];
   csvManager
     .read("member.csv")
     .then(jsonArr => {
-      sGroup = jsonArr.map(element => {
-        let arr = [];
-        arr.push(element.name);
-        arr.push(element.groupIdx);
-        return arr;
-      });
-    }) //level 1
-    .then(
-      csvManager.read("group.csv").then(arr => {
-        let test = [];
-        test = arr.map(element => {
-          let tmp = [];
-          tmp.push(element.groupIdx);
-          tmp.push(element.name);
-          return tmp;
-        });
-        sGroup.forEach(element => {
-          for (var i of test) {
-            if (element[1] === i[0]) element[1] = i[1];
-          }
-        });
-        res.send(sGroup);
-      }) //level 2
-    )
-    .catch(err => {
-      console.log(err);
-    });
+      return jsonArr;
+    })
+    //level 1
+    .then(jsonArr =>
+      csvManager
+        .read("group.csv")
+        .then(arr => {
+          jsonArr.forEach(element => {
+            for (var i of arr) {
+              if (element.groupIdx === i.groupIdx) element.groupIdx = i.name;
+            }
+          });
+          res.json(jsonArr);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    );
+  //level 2
 });
-
 router.get("/:groupIdx", function(req, res, next) {
   let groupId = req.params.groupIdx;
   let idxGroup = [];
